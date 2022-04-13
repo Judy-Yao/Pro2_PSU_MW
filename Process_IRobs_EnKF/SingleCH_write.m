0c5f8596ebeb5a3c391a5709d13039b88195becc
Judy-Yao <yao.zhu.91@gmail.com>
2022-04-13 18:24:42 -0400

    Finish thinning IR data!

---
+++ Process_IRobs_EnKF/SingleCH_write.m
@@ -0,0 +1,31 @@
+function [] = SingleCH_write(istorm, Tb_file, DAtime, min_WRF_lat, max_WRF_lat, min_WRF_lon, max_WRF_lon, control)
+
+    disp(["  File: " + Tb_file]);
+    
+    [~, filename, ~] = fileparts(Tb_file);
+    file_info = split(filename,'_'); Ch = str2num(erase(file_info(6),'CH'));
+    [myTimes,mySat_name,myChNum,myLat,myLon,myTb,myROI_hydro,myROI_other,myObsErr,mySat_alt] = ProduceForEnKF(DAtime, Ch, min_WRF_lat, max_WRF_lat, min_WRF_lon, max_WRF_lon, Tb_file, control);
+     
+    % create text file name
+    filename = strcat(control.output_dir,control.storm_phase{istorm},'/radiance_d03_',DAtime,'_so');
+    disp("  Output processed GOESR obs file: "+filename);
+    formatSpec = '%12s%12s%12i%12.3f%12.3f%12.3f%12i%12i%12.3f%12.3f\n';
+    fileID = fopen(filename,'w');
+    % reshape values into columns
+    out_times = cat(1, myTimes{:}); 
+    out_sat_name = cat(1, mySat_name{:}); out_chNum = cat(1, myChNum{:});
+    out_lat = cat(1, myLat{:}); out_lon = cat(1, myLon{:}); out_Tb = cat(1, myTb{:});
+    out_ROI_hydro = cat(1, myROI_hydro{:}); out_ROI_other = cat(1, myROI_other{:});
+    out_obsErr = cat(1, myObsErr{:}); out_Sat_alt = cat(1, mySat_alt{:});
+
+    % write values to a file
+    for rd = 1:length(out_Tb)
+        fprintf(fileID, formatSpec, ...
+                out_times(rd), out_sat_name(rd), out_chNum(rd),...
+                out_lat(rd), out_lon(rd), out_Tb(rd), ...
+                out_ROI_hydro(rd), out_ROI_other(rd), out_obsErr(rd), out_Sat_alt(rd));
+    end
+    
+    fclose(fileID);
+
+end
--- Process_IRobs_EnKF/main_process_IRobs.m
+++ Process_IRobs_EnKF/main_process_IRobs.m
@@ -11,7 +11,7 @@ control.storm_phase = {'MariaRI',};
 control.period = {{'201709160000','201709180000'},};
 %control.period = {{'201709030600','201709050600'},{'201709050600','201709070600'},{'201709160000','201709180000'}}; %YYYYMMDDHHmm
 % ---Satellite information
-control.favCH = {'CH8', };
+control.favCH = [8,];
 control.facWL = {'6.2um', };
 % --- WRF setup
 control.nx = 297; % number of grid points along X direction
@@ -22,7 +22,9 @@ control.domain_buffer = 1.05; % scaling factor (Enlarge the domain 3 a bit so th
 control.filter_reso = [18;12]; 
 % 18-by-18 km box with a 200-km radius of inﬂuence for non-hydro variables; 12-by-12 km box with a 30-km radius of inﬂuence for all variables
 control.roi_oh = {[200,0], [30,30]}; % roi [other variables, hydrometeors]
-control.obsError = [3 3];
+control.obsError = 3;
+control.Sat_alt = 35000; % km
+
 
 % ---------- Loop through each storm object -------------------
 for istorm = 1:length(control.storm_phase)
@@ -52,24 +54,44 @@ for istorm = 1:length(control.storm_phase)
 
    % --- Collect GOESR Obs files within the period of interest into a directory
    disp('Collecting useful GOES-R obs files for this study......');
-   [all_Tbfile_name] = Collect_GOESR(istorm, bestrack_str, control);
- 
+   Collect_GOESR(istorm, bestrack_str, control);
 
-   % --- Loop through each useful Tb file via a symbolic link
+   % --- Loop through each DAtime and output obs
    Tb_dir = [control.obs_collect_dir,control.storm_phase{istorm},'/*'];
    Tb_files = strsplit(ls(Tb_dir));
    Tb_files = Tb_files(~cellfun('isempty',Tb_files)); % get rid of annoying empty cell    
 
-   for iTb = 1:length(Tb_files)
-        Tb_file = Tb_files{iTb};
-        %[filepath,filename,filext] = fileparts(Tb_file);            
-        %idx_collectedTb = find([filename,filext] == all_Tbfile_name);
-        Write_toEnKF(Tb_file, control);                
-        %Singlepass_write(idx_collectedTb,istorm,Swath_used,ChIdx_all,ChName_all,DAtime_all,loc_DAtime_all,Tb_file,control);
-    
+   for it = 1:size(bestrack_str,1)
+        files = [];
+        for iTb = 1:length(Tb_files)
+            Tb_file = Tb_files{iTb};
+            if contains(Tb_file, bestrack_str{it,1})
+                files = [files, string(Tb_file)];
+            else
+                continue;
+            end
+        end
+        
+        DAtime = bestrack_str{it,1};
+        disp(["Dealing with file(s) at DAtime: " + DAtime]);
 
+        % Create the domain centered at the best-track location at the DA time 
+        nx = control.nx*control.domain_buffer; % zoom out
+        ny = control.ny*control.domain_buffer; % zoom out
+        % Note: below algorithm works if only for all channels of interest, the DA_time are the same
+        DA_btk = bestrack_str{it,3}; % best-track location at the DA time
+        min_WRF_lat = DA_btk(1) - (ny/2*control.dx)/(cos(DA_btk(1)*(pi/180))*111);
+        max_WRF_lat = DA_btk(1) + (ny/2*control.dx)/(cos(DA_btk(1)*(pi/180))*111);
+        min_WRF_lon = DA_btk(2) - (nx/2*control.dx)/111;
+        max_WRF_lon = DA_btk(2) + (nx/2*control.dx)/111;
+        disp(['  min of xlong: ',num2str(min_WRF_lon), ', max of xlong: ',num2str(max_WRF_lon)]);
+        disp(['  min of xlat: ',num2str(min_WRF_lat), ', max of xlat: ',num2str(max_WRF_lat)]);
 
-
+        if length(files) == 1
+            SingleCH_write(istorm, files, DAtime, min_WRF_lat, max_WRF_lat, min_WRF_lon, max_WRF_lon, control);                
+        else
+            MultiCh_write(istorm, files, DAtime, min_WRF_lat, max_WRF_lat, min_WRF_lon, max_WRF_lon, control);
+        end
    end
 
 end
