1e8a2cb6d72887d27d65c47542311ab47c140729
Judy-Yao <yao.zhu.91@gmail.com>
2022-09-19 13:50:20 -0500

    Vis Tb

---
+++ Post_WRF_EnKF/Vis/Tb/IR_functions.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python                                                                                                                                      
+
+from netCDF4 import Dataset
+from wrf import getvar,interplevel
+import numpy as np
+import scipy as sp
+import scipy.ndimage
+import datetime
+from matplotlib.dates import DayLocator, HourLocator, DateFormatter, drange
+from math import pi
+from matplotlib.colors import LinearSegmentedColormap as LSC
+import sys
+
+Req = 6378137.
+Rpol = 6356752.31414
+H = 42164160.
+ramda_o = -89.5*pi/180.
+def read_GOES16(filename, lonlat=True):
+    Tb_dict = {}
+    print(filename)
+    ncfile = Dataset(filename)
+    tb = ncfile.variables['CMI'][:]
+    Tb_dict['tb'] = tb[::-1,:]
+    if lonlat:
+        x = ncfile.variables['x'][:]
+        y = ncfile.variables['y'][:]
+        xx, yy = np.meshgrid(x,y)
+        a = np.sin(xx)**2 + np.cos(xx)**2 * (np.cos(yy)**2 + Req**2/Rpol**2 * np.sin(yy)**2)
+        b = - 2.* H * np.cos(xx) * np.cos(yy)
+        c = H**2 - Req**2
+        Rs = (-b - np.sqrt(b**2 - 4.*a*c)) / (2.*a)
+        Sx = Rs * np.cos(xx) * np.cos(yy)
+        Sy = -Rs * np.sin(xx)
+        Sz = Rs * np.cos(xx) * np.sin(yy)
+        lats = np.arctan( Req**2/Rpol**2 * Sz / np.sqrt((H - Sx)**2 + Sy**2))
+        lons = ramda_o - np.arctan(Sy / (H - Sx))
+        Tb_dict['lons'] = lons[::-1,:]/pi*180.
+        Tb_dict['lats'] = lats[::-1,:]/pi*180.
+
+    ncfile.close()
+    return Tb_dict
+
+def read_crtm(filename,xmax,ymax,ch_list):
+    print(filename)
+    data = np.fromfile(filename,dtype='>f4')
+    n_ch = len(data)/(xmax*ymax)-2
+    n_ch = int(n_ch)
+    #print('n_ch:',type(n_ch))
+    #print('xmax', type(xmax))
+    if n_ch != len(ch_list):
+        print('Error!! # of channels in data is '+str(n_ch))
+    sim = data[:].reshape(n_ch+2,ymax,xmax)
+    Tb_dict = {}
+    Tb_dict['lons'] = sim[0,:,:]
+    Tb_dict['lats'] = sim[1,:,:]
+    for rec in range(n_ch):
+        Tb_dict[ch_list[rec]] = sim[rec+2,:,:]
+    return Tb_dict
+
+def ir_colormap(vmax=325): 
+        ## for 173.15 K ~ 333.15 K
+        vmin = 185  
+        clevs = 0.5       
+        interval = np.arange(vmin,vmax+clevs,clevs)
+        clevs = 20        
+        ticks = np.arange(180,int(vmax)+clevs,clevs)
+        theresholds = [15., 20., 30., 40., 50., 55., 60., 70.]
+        cdict = {'red':  ((0.0,    0.9297, 0.9297), # violet
+                          (theresholds[0]/(float(vmax)-vmin), 0.1   , 0.1   ), # black red
+                          (theresholds[1]/(float(vmax)-vmin), 0.4844, 0.4844), # Barn red
+                          (theresholds[2]/(float(vmax)-vmin), 1.0   , 1.0   ), # red
+                          (theresholds[3]/(float(vmax)-vmin), 1.0   , 1.0   ), # yellow
+                          (theresholds[4]/(float(vmax)-vmin), 0.0   , 0.0   ), # green
+                          (theresholds[5]/(float(vmax)-vmin), 0.0977, 0.0977), # midnight blue
+                          (theresholds[6]/(float(vmax)-vmin), 0.0   , 0.0   ), # blue
+                          (theresholds[7]/(float(vmax)-vmin), 1.0   , 1.0   ), # white
+                          (1.0,    0.0   , 0.0   )),# black
+                'green':((0.0000, 0.5078, 0.5078),
+                         (theresholds[0]/(float(vmax)-vmin),0.0   , 0.0   ),
+                         (theresholds[1]/(float(vmax)-vmin),0.0391, 0.0391),
+                         (theresholds[2]/(float(vmax)-vmin),0.0   , 0.0   ),
+                         (theresholds[3]/(float(vmax)-vmin),1.0   , 1.0   ),
+                         (theresholds[4]/(float(vmax)-vmin),1.0   , 1.0   ),
+                         (theresholds[5]/(float(vmax)-vmin),0.0977, 0.0977),
+                         (theresholds[6]/(float(vmax)-vmin),0.0   , 0.0   ),
+                         (theresholds[7]/(float(vmax)-vmin),1.0   , 1.0   ),
+                         (1.0,    0.0   , 0.0   )),
+                'blue': ((0.0000, 0.9297, 0.9297),
+                         (theresholds[0]/(float(vmax)-vmin),0.0   , 0.0   ),
+                         (theresholds[1]/(float(vmax)-vmin),0.0078, 0.0078),
+                         (theresholds[2]/(float(vmax)-vmin),0.0   , 0.0   ),
+                         (theresholds[3]/(float(vmax)-vmin),0.0   , 0.0   ),
+                         (theresholds[4]/(float(vmax)-vmin),0.0   , 0.0   ),
+                         (theresholds[5]/(float(vmax)-vmin),0.4375, 0.4375),
+                         (theresholds[6]/(float(vmax)-vmin),1.0   , 1.0   ),
+                         (theresholds[7]/(float(vmax)-vmin),1.0   , 1.0   ),
+                         (1.0,    0.0   , 0.0   ))
+                }
+        ircmap = LSC('ircmap',cdict)
+        ircmap.set_under('white')
+        return ircmap, vmin, vmax, interval, ticks
+
+
+
+
+
+
+
+
+
+
+
---
+++ Post_WRF_EnKF/Vis/Tb/incomplete_Tbspace_compare_MW_alltxt.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import cm
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+
+#from cartopy import crs as ccrs
+
+F_Obs = 'microwave_d03_201708221300_so.txt'
+F_Hxb_mean = 'wrf_enkf_input_d03_mean.tb.ssmis_f17.crtm.conv.txt'
+F_Hxa_mean = 'wrf_enkf_output_d03_mean.tb.ssmis_f17.crtm.conv.txt'
+#def SSMIS(F_Obs, F_Hxb_mean, F_Hxa_mean):
+tmp = np.fromfile(F_Hxb_mean, sep=' ')
+ncol = 8
+
+Lat_all = tmp[0::ncol] #latitude
+Lon_all = tmp[1::ncol] #longitude
+Chnum_all = tmp[2::ncol] #channel
+Yo_all  = tmp[3::ncol] #observedTB
+Yb_all  = tmp[4::ncol] #BackgroundTB    
+
+tmp = np.fromfile(F_Hxa_mean, sep=' ') 
+Ya_all = tmp[4::ncol]
+
+f, ax=plt.subplots(2, 3, dpi=300) #, linewidth=0.5, sharex='all', sharey='all',  dpi=300)
+#f, ax=plt.subplots(2, 3, subplot_kw={'projection': ccrs.PlateCarree()}, linewidth=0.5, sharex='all', sharey='all',  dpi=300)
+
+# customize colormap
+max_T = 300
+min_T = 80
+min_Jet = 150
+jetLength = max_T - min_Jet + 1
+notJetLength = (min_Jet - 1) - (min_T + 1) + 1 
+
+jet = cm.get_cmap('jet', max_T-min_T)
+Myjet = jet(np.linspace(0,1,jetLength))
+
+jet_red = Myjet[:,0]
+jet_green = Myjet[:,1]
+jet_blue = Myjet[:,2]
+
+jetAdd_red = np.linspace(1.0, jet_red[0], notJetLength)
+jetAdd_green = np.linspace(1.0, jet_green[0], notJetLength)
+jetAdd_blue  = np.linspace(1.0, jet_blue[0], notJetLength) 
+
+cm_red = np.concatenate([np.insert(jetAdd_red,0,0),np.append(jet_red,0)])
+cm_green = np.concatenate([np.insert(jetAdd_green,0,0),np.append(jet_green,0)])
+cm_blue = np.concatenate([np.insert(jetAdd_blue,0,0),np.append(jet_blue,0)])
+
+newJet_value = np.column_stack([cm_red,cm_green,cm_blue, np.ones(len(cm_red))])
+
+newJet = ListedColormap(newJet_value)
+
+ch_num = [13, 9]
+scatter_size = [2.5, 2.5]
+
+
+lat = np.array(Lat_all)
+lon = np.array(Lon_all)
+lat_min = np.amin(lat)
+lat_max = np.amax(lat)
+lon_min = np.amin(lon)
+lon_max = np.amax(lon)
+
+for i in range(2):
+    idx = Chnum_all == ch_num[i]
+ #   ax[i,0].set_extent([lon_min, lon_max, lat_min, lat_max], crs=ccrs.PlateCarree())
+ #   ax[i,0].coastlines(resolution='10m', color='black',linewidth=0.5)
+    cs = ax[i,0].scatter(Lon_all[idx],Lat_all[idx],scatter_size[i],c=Yo_all[idx],edgecolors='none', cmap=newJet, vmin=min_T, vmax=max_T) #,i transform=ccrs.PlateCarree())
+    
+ #   ax[i,1].set_extent([lon_min, lon_max, lat_min, lat_max], crs=ccrs.PlateCarree())
+ #   ax[i,1].coastlines(resolution='10m', color='black',linewidth=0.5)
+    cs = ax[i,1].scatter(Lon_all[idx],Lat_all[idx],scatter_size[i],c=Yb_all[idx],edgecolors='none', cmap=newJet, vmin=min_T, vmax=max_T)#, transform=ccrs.PlateCarree()) 
+
+#    ax[i,2].set_extent([lon_min, lon_max, lat_min, lat_max], crs=ccrs.PlateCarree())
+ #   ax[i,2].coastlines(resolution='10m', color='black',linewidth=0.5)
+    cs = ax[i,2].scatter(Lon_all[idx],Lat_all[idx],scatter_size[i],c=Ya_all[idx],edgecolors='none', cmap=newJet, vmin=min_T, vmax=max_T) #, transform=ccrs.PlateCarree())
+
+#plt.xlim([-91,-84])
+#plt.ylim([15,24])
+
+# Colorbar
+caxes = f.add_axes([0.2, 0.97, 0.4, 0.02])
+cbar = f.colorbar(cs, orientation="horizontal", cax=caxes)
+cbar.ax.tick_params(labelsize=5)
+#plt.text( 0.8, 0.7, 'Brightness Temperature/K', fontsize=4)
+
+
+#subplot title
+font = {'size':6,}
+
+ax[0,0].set_title('Yo', font, fontweight='bold')
+ax[0,1].set_title('HXb', font, fontweight='bold')
+ax[0,2].set_title('HXa', font, fontweight='bold')
+
+plt.subplots_adjust(wspace=0, hspace=0)
+
+plt.savefig('201708221200.png', dpi=300)
+    
+
+#if __name__ == '__main__':
+    # Path
+    #F_Obs = 'microwave_d03_201708221200_so.txt'
+    #F_Hxb_mean = 'wrf_enkf_input_d03_mean.tb.ssmis_f17.crtm.conv.txt'
+    #F_Hxa_mean = 'wrf_enkf_output_d03_mean.tb.ssmis_f17.crtm.conv.txt' 
+
+    #SSMIS(F_Obs, F_Hxb_mean, F_Hxa_mean)
