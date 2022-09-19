e0727fad330bdacfdcdd167667adc367a48682be
Judy-Yao <yao.zhu.91@gmail.com>
2022-09-19 13:52:09 -0500

    Track

--- Post_WRF_EnKF/Vis/Model/Track_intensity.py
+++ Post_WRF_EnKF/Vis/Model/Track_intensity.py
@@ -20,15 +20,16 @@ from matplotlib import pyplot as plt
 from cartopy import crs as ccrs
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 
+matplotlib.rcParams['lines.linewidth'] = 1
+matplotlib.rcParams['lines.markersize'] = 2
+matplotlib.rcParams['lines.markeredgewidth'] = 0
+matplotlib.rcParams['font.size'] = 6
 
 matplotlib.rcParams['xtick.direction'] = 'in'
 matplotlib.rcParams['ytick.direction'] = 'in'
 matplotlib.rcParams['xtick.top'] = True
 matplotlib.rcParams['ytick.right'] = True
-matplotlib.rcParams['lines.linewidth'] = 1.5
-matplotlib.rcParams['lines.markersize'] = 2
-matplotlib.rcParams['lines.markeredgewidth'] = 0
-matplotlib.rcParams['font.size'] = 6
+
 
 # Read the post-storm best-track file of the storm at all times
 def read_bestrack(Storm):
@@ -70,13 +71,13 @@ def read_bestrack(Storm):
 
 
 # Return records in the complete best-track file at times of interest 
-def btk_in_duration(Storm, Btk_start, Btk_end):
+def btk_in_duration(Storm, DF_start, DF_end):
     
     # Calculate the duration from the start to the end of deterministic forecast in hour
-    Btk_diff = datetime.strptime(Btk_end,"%Y%m%d%H%M") - datetime.strptime(Btk_start,"%Y%m%d%H%M") 
-    Btk_diff_hour = Btk_diff.total_seconds() / 3600 
+    DF_diff = datetime.strptime(DF_end,"%Y%m%d%H%M") - datetime.strptime(DF_start,"%Y%m%d%H%M") 
+    DF_diff_hour = DF_diff.total_seconds() / 3600 
     # List the times (in string format) every 6 hours in the duration 
-    time_interest_dt = [datetime.strptime(Btk_start,"%Y%m%d%H%M") + timedelta(hours=t) for t in list(range(0, int(Btk_diff_hour), 6))]
+    time_interest_dt = [datetime.strptime(DF_start,"%Y%m%d%H%M") + timedelta(hours=t) for t in list(range(0, int(DF_diff_hour), 6))]
     time_interest_str = [time_dt.strftime("%Y%m%d%H%M") for time_dt in time_interest_dt]
     # Read the complete best-track file
     dict_all_btk = read_bestrack(Storm)    
@@ -103,7 +104,7 @@ def btk_in_duration(Storm, Btk_start, Btk_end):
 
 
 # Get track/intensity from model output
-def read_wrfout(Storm, Exper_name, directory, filename_pickle=None, force_reload=False):
+def read_wrfout(directory, filename_pickle=None, force_reload=False):
     if filename_pickle is None:
         filename_pickle = directory + '/HPI.pickle'
     if not os.path.exists( filename_pickle ) or force_reload:
@@ -143,97 +144,16 @@ def read_wrfout(Storm, Exper_name, directory, filename_pickle=None, force_reload
     return HPI
 
 
-# Get track and intensity from ATCF part in rsl.error.0000
-def read_rsl_error(Storm, Exper_name, directory, DF_start, DF_end):
-    with open(directory + '/ATCF_rsl.error.0000') as f:
-        all_lines = f.readlines()
-
-    # Read and process records
-    DF_start_real = [] # if use the option "time_to_move" in WRF, DF_start_real is not equal to DF_start
-    time_all = []
-    lat_all = []
-    lon_all = []
-    maxV_all = []
-    minP_all = []
-
-    num_line = 0
-    for line in all_lines:
-        # Split 
-        split_line = line.split()
-        # time
-        time_wrf = split_line[1]
-        time_dt = datetime.strptime( time_wrf,"%Y-%m-%d_%H:%M:%S" )
-        time_all.append( time_dt.strftime( "%Y%m%d%H%M" ) )
-        if num_line == 0:
-            DF_start_real.append( time_dt.strftime( "%Y%m%d%H%M" ) )
-
-        # lat
-        lat_all.append( float(split_line[2]) )
-        # lon
-        lon_all.append( float(split_line[3]) )
-        # minimum slp
-        minP_all.append( float(split_line[4]) )
-        # maximum wind speed
-        maxV_all.append( float(split_line[5])*0.51444 )
-        num_line = num_line + 1
-
-    # Only select a subset of HPI records
-    # Calculate the duration from the start to the end of deterministic forecast in hour
-    DF_start = DF_start_real[0]
-    DF_diff = datetime.strptime(DF_end,"%Y%m%d%H%M") - datetime.strptime(DF_start,"%Y%m%d%H%M")
-    DF_diff_hour = DF_diff.total_seconds() / 3600
-    # List the times (in string format) every hour in the duration 
-    time_interest_dt = [datetime.strptime(DF_start,"%Y%m%d%H%M") + timedelta(hours=t) for t in list(range(0, int(DF_diff_hour), 1))]
-    time_interest_str = [time_dt.strftime("%Y%m%d%H%M") for time_dt in time_interest_dt]
-    # Get the indices in the best-track file corresponded to the times of interest
-    idx_sbs = []
-
-    lat_sbs = []
-    lon_sbs = []
-    max_ws_sbs = []
-    min_slp_sbs = []
-    for time_str in time_interest_str:
-        boolean_compare = [ eachT  == time_str for eachT in time_all ]
-        if any(boolean_compare):
-            idx = int( np.where(boolean_compare)[0] )
-            idx_sbs.append(idx)
-            lat_sbs.append( lat_all[idx] )
-            lon_sbs.append( lon_all[idx] )
-            max_ws_sbs.append( maxV_all[idx] )
-            min_slp_sbs.append( minP_all[idx] )
-
-    dict_model = {'time': time_interest_str, 'lat': lat_sbs, 'lon': lon_sbs, 'max_ws': max_ws_sbs, 'min_slp': min_slp_sbs} 
-
-    return dict_model
+def plot_one( ax0, ax1, ax2,  state, linestyle, label, step=1):
+    dates = [datetime.strptime(i,"%Y%m%d%H%M") for i in state['time']]
+    #hours = np.array([date.hour for date in dates])
+    #idx = np.mod(hours.astype(int), step) == 0
+    ax0.plot(state['lon'], state['lat'], linestyle, label=label, transform=ccrs.PlateCarree())
+    ax1.plot_date(dates, state['min_slp'], linestyle, label=label)
+    ax2.plot_date(dates, state['max_ws'], linestyle, label=label)
 
 
-def plot_one( ax0, ax1, ax2,  state, color, label, step=1):
-   
-    if label == 'Best track':
-        times = state['time']
-        lon = state['lon']
-        lat = state['lat']
-    else:
-        times = state['time'][::6]
-        lon = state['lon'][::6]
-        lat = state['lat'][::6]
-   
-    ax0.plot(lon, lat, marker='o', markersize=2, color=color,linewidth=1, label=label, transform=ccrs.PlateCarree())
-    
-    idx = 0
-    for it in times:
-        if it[8:10] == '00':
-            ax0.scatter( lon[idx], lat[idx],s=5, marker='o',edgecolor="white",transform=ccrs.PlateCarree())    
-            ax0.annotate(it[6:8], xy=(lon[idx], lat[idx]),  xycoords='data', transform=ccrs.PlateCarree())
-            #ax0.text((state['lon'][idx], state['lat'][idx],it[6:8], fontsize=4,transform=ccrs.PlateCarree())
-        idx = idx + 1
-
-    dates = [datetime.strptime(i,"%Y%m%d%H%M") for i in state['time']] 
-    ax1.plot_date(dates, state['min_slp'], color, label=label)
-    ax2.plot_date(dates, state['max_ws'], color, label=label)
-
-
-def plot_hpi(Storm, wrf_dir, read_HPI_wrfout, domain_range, output_dir=None):
+def plot_hpi(Strom, domain_range, output_dir=None):
     reload_data = False
     step = 1
 
@@ -247,48 +167,46 @@ def plot_hpi(Storm, wrf_dir, read_HPI_wrfout, domain_range, output_dir=None):
     ax1 = fig.add_subplot( gs[0,1] )
     ax2 = fig.add_subplot( gs[0,2] )
 
+<<<<<<< HEAD:Post_WRF_EnKF/Vis/Model/Track_intensity_IRMW.py
     # Plot HPI from post-storm analysis
-    #Btk_start = '201708221200'
-    #Btk_end = '201708270000'
-    Btk_start = '201709161800'#'201708221200'#'201709030600'
-    Btk_end = '201709210600'#'201708270000'#'201709090000'
+    Btk_start = '201709030600'#'201708221200' #'201709030600'
+    Btk_end = '201709090000'#'201708270000'#'201709090000'
     best_track = btk_in_duration(Storm, Btk_start, Btk_end)
-    plot_one ( ax0, ax1, ax2, best_track,  'black', 'Best track', step=step )
+    plot_one ( ax0, ax1, ax2, best_track,  'k-', 'Best track', step=step )
 
     # Plot HPI from deterministic forecasts
-    #DF_model_end  = '201708270000'
-    DF_model_end  = '201709210600'
-    IR_init_times = sorted(os.listdir(  wrf_dir+'/'+Storm+'/newWRF_IR_only/wrf_df/' ))
-    IRMW_init_times = sorted(os.listdir(  wrf_dir+'/'+Storm+'/newWRF_MW_THO/wrf_df/' ))
-    
-    IRMW_color = [ '#FFB5A6','#FF998B','#E36359','#C44841','#A62C2B']
-    IR_color = [ '#BAE4B5', '#82CEA9', '#63AD9C', '#167856']
+    DF_model_end  = '201709090000' #''201708270000'
+    DF_init_times = sorted(os.listdir(  wrf_dir+'/'+Storm+'/'+Exper_name+'/wrf_df/' ))
+
+    color_model = [ '#b8d5cd', '#8abaae', '#5ca08e', '#2e856e', '#006a4e']
 
     if read_HPI_wrfout == True:
         i = 0
         for init_time in DF_init_times:
-            plot_one( ax0, ax1, ax2, read_wrfout(Storm, wrf_dir+'/'+Storm+'/newWRF_MW_THO/wrf_df/'+init_time, force_reload=reload_data), color_model[i], 'IR+MW: '+ init_time, step=step)
+            plot_one( ax0, ax1, ax2, read_wrfout(Storm, Exper_name, wrf_dir+'/'+Storm+'/'+Exper_name+'/wrf_df/'+init_time, force_reload=reload_data), color_model[i], 'IR+MW: '+ init_time, step=step)
             i = i+1
-
     elif read_HPI_wrfout == False:
         i = 0
-        for init_time in IR_init_times:
-            plot_one( ax0, ax1, ax2, read_rsl_error(Storm, 'newWRF_IR_only', wrf_dir+'/'+Storm+'/newWRF_IR_only/wrf_df/'+init_time, init_time, DF_model_end), IR_color[i], 'IR: '+ init_time, step=step)
+        for init_time in DF_init_times:
+            plot_one( ax0, ax1, ax2, read_rsl_error(Storm, Exper_name, wrf_dir+'/'+Storm+'/'+Exper_name+'/wrf_df/'+init_time, init_time, DF_model_end), color_model[i], 'IR+MW: '+ init_time, step=step)
             i = i+1
-        
-        i = 0
-        for init_time in IRMW_init_times:
-            plot_one( ax0, ax1, ax2, read_rsl_error(Storm, 'newWRF_MW_THO', wrf_dir+'/'+Storm+'/newWRF_MW_THO/wrf_df/'+init_time, init_time, DF_model_end), IRMW_color[i], 'IR+MW: '+ init_time, step=step)
-            i = i+1 
+=======
+    # Plot
+    plot_one( ax0, ax1, ax2, read_wrfout('/work2/06191/tg854905/stampede2/Pro2_PSU_MW/HARVEY/MW_THO/wrf_df/201708221200/mean/', force_reload=reload_data), 'b-', 'IR+MW: 201708221200', step=step)
+    plot_one( ax0, ax1, ax2, read_wrfout('/work2/06191/tg854905/stampede2/Pro2_PSU_MW/HARVEY/MW_THO/wrf_df/201708221800/mean/', force_reload=reload_data), 'g-', 'IR+MW: 201708221800', step=step)
 
-
+    DF_start = '201708221200'
+    DF_end = '201708270000'
+    best_track = btk_in_duration(Storm, DF_start, DF_end)
+    plot_one ( ax0, ax1, ax2, best_track,  'k-', 'Best track', step=step )
+>>>>>>> parent of e20505b... Minor detail:Post_WRF_EnKF/Vis/Model/Track_intensity.py
 
     # Set labels
-    lon_ticks = list(range(math.ceil(domain_range[0]), math.ceil(domain_range[1]), 4))
+    lon_ticks = list(range(math.ceil(domain_range[0]), math.ceil(domain_range[1]), 2))
     lat_ticks = list(range(math.ceil(domain_range[2]), math.ceil(domain_range[3]), 2))
     gl = ax0.gridlines(crs=ccrs.PlateCarree(), draw_labels=False,linewidth=0.1, color='gray', alpha=0.5, linestyle='--')
-    gl.ylabels_left = True
-    gl.xlabels_bottom = True
+    gl.left_labels = True
+    gl.bottom_label = True
     gl.ylocator = mticker.FixedLocator(lat_ticks)
     gl.xlocator = mticker.FixedLocator(lon_ticks)
     gl.yformatter = LATITUDE_FORMATTER
@@ -296,12 +214,15 @@ def plot_hpi(Storm, wrf_dir, read_HPI_wrfout, domain_range, output_dir=None):
     gl.xlabel_style = {'size': 6}
     gl.ylabel_style = {'size': 6}  
     
+<<<<<<< HEAD:Post_WRF_EnKF/Vis/Model/Track_intensity_IRMW.py
     #ax1.set_xlim([datetime(2017, 8, 22, 12, 0, 0), datetime(2017, 8, 27)])
-    #ax2.set_xlim([datetime(2017, 8, 22, 12, 0, 0), datetime(2017, 8, 27)]) 
-    #ax1.set_xlim([datetime(2017, 9, 3, 6, 0, 0), datetime(2017, 9, 9)])
-    #ax2.set_xlim([datetime(2017, 9, 3, 6, 0, 0), datetime(2017, 9, 9)])
-    ax1.set_xlim([datetime(2017, 9, 16, 18, 0, 0), datetime(2017, 9, 21)])
-    ax2.set_xlim([datetime(2017, 9, 16, 18, 0, 0), datetime(2017, 9, 21)])
+    #ax2.set_xlim([datetime(2017, 8, 22, 12, 0, 0), datetime(2017, 8, 27)])
+    ax1.set_xlim([datetime(2017, 9, 3, 6, 0, 0), datetime(2017, 9, 9)])
+    ax2.set_xlim([datetime(2017, 9, 3, 6, 0, 0), datetime(2017, 9, 9)])
+=======
+    ax1.set_xlim([datetime(2017, 8, 22, 12, 0, 0), datetime(2017, 8, 27)])
+    ax2.set_xlim([datetime(2017, 8, 22, 12, 0, 0), datetime(2017, 8, 27)])
+>>>>>>> parent of e20505b... Minor detail:Post_WRF_EnKF/Vis/Model/Track_intensity.py
     ax1.tick_params(axis='x', labelrotation=45)
     ax2.tick_params(axis='x', labelrotation=45)
     ax2.legend(frameon=False, loc='upper left')
@@ -311,7 +232,11 @@ def plot_hpi(Storm, wrf_dir, read_HPI_wrfout, domain_range, output_dir=None):
     ax2.set_title( 'Vmax' )
     
     if output_dir is not None:                             
-        plt.savefig('/work2/06191/tg854905/stampede2/Pro2_PSU_MW/'+Storm+'/newWRF_MW_THO/Vis_analyze/Model/'+Storm+'_all.png')
+<<<<<<< HEAD:Post_WRF_EnKF/Vis/Model/Track_intensity_IRMW.py
+        plt.savefig('/work2/06191/tg854905/stampede2/Pro2_PSU_MW/'+Storm+'/newWRF_MW_THO/Vis_analyze/Model/'+Storm+'_IRMW.png')
+=======
+        plt.savefig('./test.png')
+>>>>>>> parent of e20505b... Minor detail:Post_WRF_EnKF/Vis/Model/Track_intensity.py
         #plt.savefig('%s/hpi_fcst_%s_%s.png' % (output_dir, '201708240000', '201708270000') )                                                
     else:
         plt.show()
@@ -319,27 +244,25 @@ def plot_hpi(Storm, wrf_dir, read_HPI_wrfout, domain_range, output_dir=None):
 
 
 if __name__ == '__main__':
-    
-    #Storm = 'HARVEY'
-    Storm = 'MARIA'
+<<<<<<< HEAD:Post_WRF_EnKF/Vis/Model/Track_intensity_IRMW.py
+    Storm = 'IRMA'
+    Exper_name = 'newWRF_MW_THO'
     wrf_dir = '/scratch/06191/tg854905/Pro2_PSU_MW/'
 
     read_HPI_wrfout = False
 
-    #lon_min = -101
-    #lon_max = -86
-    #lat_min = 16
-    #lat_max = 31
-
-    #lon_min = -85
-    #lon_max = -45
-    #lat_min = 12
-    #lat_max = 30
+    lon_min = -85#-101#-85
+    lon_max = -45#-86#-45
+    lat_min = 12#16#12
+    lat_max = 30#31#30
+=======
+    Storm = 'HARVEY'
     
-    lon_min = -70
-    lon_max = -48
-    lat_min = 5
-    lat_max = 20
+    lon_min = -101
+    lon_max = -85
+    lat_min = 16
+    lat_max = 31
+>>>>>>> parent of e20505b... Minor detail:Post_WRF_EnKF/Vis/Model/Track_intensity.py
     domain_range = [lon_min, lon_max, lat_min, lat_max]
 
-    plot_hpi( Storm, wrf_dir, read_HPI_wrfout, domain_range, output_dir = './out')
+    plot_hpi( Storm, domain_range, output_dir = './out')
