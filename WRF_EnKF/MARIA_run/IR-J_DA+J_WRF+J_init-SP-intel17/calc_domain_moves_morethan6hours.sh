4706b56d9b3ef350fa40b6806aad0c1c82e2813b
Judy-Yao <yao.zhu.91@gmail.com>
2023-04-18 15:33:59 -0500

    Control script for experiments

--- WRF_EnKF/MARIA_run/IR-J_DA+J_WRF+J_init-SP-intel17/calc_domain_moves_morethan6hours.sh
+++ WRF_EnKF/MARIA_run/IR-J_DA+J_WRF+J_init-SP-intel17/calc_domain_moves_morethan6hours.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#This function generates domain_moves for namelist.input so that the nested domains follows 
+#This function generates domain_moves for namelist.input so that the nested domains follow
 #  storm center (from tcvitals data). Storm center lat/lon are calculated at each time (including
 #  start/end time for wrf and any lbc (0 6 12 18Z) time in between. The storm is assumed to move
 #  linearly between any two given location points.
@@ -8,44 +8,44 @@
 #
 #Input: date1 = wrf run start date
 #       date2 = wrf run end date
-#Output: a text file that contains namelist options related to domain moves.
+#Main Output: a text file that contains namelist options related to domain moves.
 
-. util.sh
 . $CONFIG_FILE
 
-date1=$1
-date2=$2
-outfile=$3
-
-echo "---------------------------------------------------"
-echo "Find TCvitals data that covers the period"
-echo "---------------------------------------------------"
-# Find tcvitals data that covers the period
+date1=$1 
 echo "Current cycle is at" $date1
+date2=$2
 echo "Next cycle is at" $date2
+outfile=$3
+echo "Will output the domain_moves to" $outfile
+
+echo " "
+echo "--------------------------------------------------------------------------------------"
+echo "Find TCvitals data that covers the period between the current cycle and the next cycle"
+echo "--------------------------------------------------------------------------------------"
 echo "Minutes between two consecutive TCvital records are" $TCV_INTERVAL
+echo "Using the date of current cycle, next cycle, and the interval of TCvital records to calculate the off time......"
 off1=`echo "(${date1:8:2}*60+${date1:10:2})%$TCV_INTERVAL" |bc` # string:P:L (P indicates the starting index, L is the length of the substring)
 echo "Current cycle is" $off1 "minutes off (off1) from the nearest TCvital record"
 off2=`echo "(${date2:8:2}*60+${date2:10:2})%$TCV_INTERVAL" |bc`
 echo "Next cycle is" $off2 "minutes off (off2) from the nearest TCvital record"
-echo "off1->$off1"
-echo "off2->$off2"
 fgdate1=`advance_time $date1 -$off1`
 echo "The start TCvital record (fgdate1) is at" $fgdate1
-fgdate2=`advance_time $date2 $((TCV_INTERVAL-$off2))`
+if [ `echo "${date2:8:2}%6" |bc` -eq "0" ]; then
+    fgdate2=$date2
+else
+    fgdate2=`advance_time $date2 $((TCV_INTERVAL-$off2))`
+fi
 echo "The end TCvital record (fgdate2) is at" $fgdate2
-echo "fgdate1->$fgdate1"
-echo "fgdate2->$fgdate2"
 n=$(echo "`diff_time $fgdate1 $fgdate2`/$TCV_INTERVAL" |bc)
-echo "The period between the start and end TCvital records is" $n "(360) minutes"
-echo "n->$n"
+echo "The period between the start and the end TCvital record is" $n "*(360) minutes"
 off3=`diff_time $fgdate1 $date2`
 echo "Next cycle is" $off3 "minutes off (off3) from the start TCvital record"
-echo "off3->$off3"
 
-echo "---------------------------------------------------"
+echo " "
+echo "--------------------------------------------------------------------------------------"
 echo "Read lat and lon from TCvitals "
-echo "---------------------------------------------------"
+echo "--------------------------------------------------------------------------------------"
 # Ge_time $date1 $((TCV_INTERVAL-$off1))` lat lon at each time node
 ndate=()
 nlat=()
@@ -69,14 +69,14 @@ for i in `seq 0 $n`; do
     nlon+=($(echo $(echo -${lonstr} | awk '{ print substr( $0, 1, length($0)-1 ) }')/10 |bc -l))
   fi
 done
-echo "Times are ${ndate[@]}"
-echo "Lats are" ${nlat[@]}
-echo "Lons are" ${nlon[@]}
+echo "Times are at ${ndate[@]}"
+echo "Lats are at" ${nlat[@]}
+echo "Lons are at" ${nlon[@]}
 
-echo "---------------------------------------------------"
+echo " "
+echo "--------------------------------------------------------------------------------------"
 echo "Convert the geolocation of TCvitals to index space (ij) in domain 1 (D1)"
-echo "---------------------------------------------------"
-# Interpolate if time node is not at lbc time
+echo "--------------------------------------------------------------------------------------"
 len_data=${#nlat[@]}
 end_index=$(echo $(($len_data-1)))
 
@@ -108,110 +108,164 @@ EOF
   rm -f ll2ij.ncl ij
 done
 
-
-echo "---------------------------------------------------"
+echo " "
+echo "--------------------------------------------------------------------------------------"
 echo "Generate domain_moves options"
-echo "---------------------------------------------------"
+echo "--------------------------------------------------------------------------------------"
 num_moves=0
-dt=0
+dt_from1stVital=0
 move_id=""
 move_interval=""
 move_cd_x=""
 move_cd_y=""
 echo "Current cycle is" $off1 "minutes off (off1) from the start TCvital record"
 echo "Next cycle is" $off3 "minutes off (off3) from the start TCvital record"
-
-for i in `seq 0 $((end_index-1))`; do  #For each time
-  echo "Location of storm (from TCvitals) in index space in D1 is:" "${ndate[$i]} (${ni[$i]} ${nj[$i]}) -> ${ndate[$i+1]} (${ni[$i+1]} ${nj[$i+1]})"
-  dmin=`diff_time ${ndate[$i]} ${ndate[$i+1]}`
-  echo "minutes between two records are" $dmin
-  di=`echo "(${ni[$i+1]} - ${ni[$i]})" |bc`
-  echo "i direction: two locations are off" $di "grid points in D1"
-  dj=`echo "(${nj[$i+1]} - ${nj[$i]})" |bc`
-  echo "j direction: two locations are off" $dj "grid points in D1"
+echo " "
+echo "Loop through each needed TCvitals..."
+for i in `seq 0 $((end_index-1))`; do  
+  echo '      '
+  echo "Evolution of TCvital locations in index space in D1 is:" "${ndate[$i]} (${ni[$i]} ${nj[$i]}) -> ${ndate[$i+1]} (${ni[$i+1]} ${nj[$i+1]})"
+  dmin_vitals=`diff_time ${ndate[$i]} ${ndate[$i+1]}`
+  echo "The next TCvital record is" $dmin_vitals "minutes apart."
+  echo "From this time to next time..."
+  di_vitals=`echo "(${ni[$i+1]} - ${ni[$i]})" |bc`
+  echo "i direction: two locations are off" $di_vitals "grid points in D1"
+  dj_vitals=`echo "(${nj[$i+1]} - ${nj[$i]})" |bc`
+  echo "j direction: two locations are off" $dj_vitals "grid points in D1"
   for i in `seq 2 $((MAX_DOM-1))`; do #Convert from D01 to smallest domain's parent domain for move steps
     echo "For D$i..."
-    di=`echo "$di*${GRID_RATIO[$i-1]}" |bc`
-    echo "i direction: two locations are off" $di "grid points in D$i" 
-    dj=`echo "$dj*${GRID_RATIO[$i-1]}" |bc`
-    echo "j direction: two locations are off" $dj "grid points in D$i"
+    di_vitals=`echo "$di_vitals*${GRID_RATIO[$i-1]}" |bc`
+    echo "i direction: two locations are off" $di_vitals "grid points in D$i" 
+    dj_vitals=`echo "$dj_vitals*${GRID_RATIO[$i-1]}" |bc`
+    echo "j direction: two locations are off" $dj_vitals "grid points in D$i"
   done
-  s=$(echo "`abs $di` + `abs $dj`" |bc)
-  echo "Two locations are off in absoluate value:" $s "grid points in D$i" 
-
-  if [ $s -gt 0 ]; then  # If displacement is non-zero, move the domain
+  d_ij_vitals=$(echo "`abs $di_vitals` + `abs $dj_vitals`" |bc)
+  echo "Two locations are off in absoluate value:" $d_ij_vitals "grid points in D$i" 
+  
+  echo '-------------------------------------------------'
+  if [ $d_ij_vitals -gt 0 ]; then  # If displacement is non-zero, move the domain
     echo "The displacement is not zero, moving the domain......"
-    echo "For" $dmin "minutes, the storm moves" $s "steps. Every step takes" $(echo $(($dmin/$s))) "minutes"
-    for t in `seq 1 $s`; do
-      echo "Moving" $t "th step..."
-      if [ $t -lt $s ]; then #break up move steps
-        dt=`echo "$dt + $dmin/$s" |bc`
-        echo "Timing:" $dt "minutes (from the start storm location/TCvital records)"
+    echo "For" $dmin_vitals "minutes, the storm moves" $d_ij_vitals "steps. Every step takes" $(echo $(($dmin_vitals/$d_ij_vitals))) "minutes"
+    echo "Note: in the current algorithm, each step represents ONE grid point!! " 
+    for step in `seq 1 $d_ij_vitals`; do
+      echo '       '
+      echo "Setting up the" $step "th step..."
+      if [ $step -lt $d_ij_vitals ]; then #break up move steps
+        dt_from1stVital=`echo "$dt_from1stVital + $dmin_vitals/$d_ij_vitals" |bc`
+        echo $dt_from1stVital "minutes ahead from the start TCvital record"
       else
-        dt=`echo "$dt + $dmin - ($s-1)*$dmin/$s" |bc`
-        echo "Timing:" $dt "minutes (from the start storm location/TCvital records)"
+        dt_from1stVital=`echo "$dt_from1stVital + $dmin_vitals - ($d_ij_vitals-1)*$dmin_vitals/$d_ij_vitals" |bc`
+        echo $dt_from1stVital "minutes ahead from the start TCvital record"
       fi
 
-      if [ $off1 -lt $dt ] && [ $dt -le $off3 ]; then
-        echo "The storm (from TCvitals) is moving between the current cycle and the next cycle..."
+      if [ $off1 -lt $dt_from1stVital ] && [ $dt_from1stVital -le $off3 ]; then
+        echo "The time associated with the step is ahead of the current cycle and behind the next cycle. Calculate the preset move....."
         num_moves=$((num_moves+1))
         move_id="$move_id $MAX_DOM,"
         dummy_step=`echo "($TIME_STEP + 59)/60" |bc` #Adjust for timestep (Division only keeps the integer part eg., 10/3 = 3)
-        if [ $dt -ge `expr $off3 - $dummy_step` ]; then #if domain moves after last time step move earlier 
-         echo "dummy_step->$dummy_step"
-          interval=$(echo $(($dt - $off1 - $dummy_step)))
-          echo "Storm is $interval minutes in advance from the current cycle"
+        if [ $dt_from1stVital -ge `expr $off3 - $dummy_step` ]; then
+         echo "The step is very close to the next cycle! Customizing the move interval..." 
+          interval=$(echo $(($dt_from1stVital - $off1 - $dummy_step)))
+          echo "The step is $interval minutes in advance from the current cycle"
           move_interval="$move_interval $interval,"
         else
-          interval=$(echo $(($dt - $off1)))
-          echo "Storm is $interval minutes in advance from the current cycle"
+          interval=$(echo $(($dt_from1stVital - $off1)))
+          echo "The step is $interval minutes in advance from the current cycle"
           move_interval="$move_interval $interval,"
         fi
 
-        if [ `abs $di` -gt 0 ] && [ `abs $dj` -gt 0 ]; then
-          echo "The storm moves both in i and j direction between the two TCvital records"
-          if [ `abs $di` -ge `abs $dj` ]; then
-            echo "The storm moves further in i direction thatn in j direction"
-            ds=$(echo "$s/`abs $dj`" |bc)
-            if [ `echo "$t % $ds" |bc` == 0 ] && [ $t -le $(echo "`abs $dj` * $ds" |bc) ]; then
+        if [ `abs $di_vitals` -gt 0 ] && [ `abs $dj_vitals` -gt 0 ]; then
+          echo "For this step, the move happens in both i and j direction between the two TCvital records"
+          if [ `abs $di_vitals` -ge `abs $dj_vitals` ]; then
+            echo "The moving distance in i direction is larger in j direction"
+            ds=$(echo "$d_ij_vitals/`abs $dj_vitals`" |bc)
+            if [ `echo "$step % $ds" |bc` == 0 ] && [ $step -le $(echo "`abs $dj_vitals` * $ds" |bc) ]; then
               move_cd_x="$move_cd_x 0,"
-              move_cd_y="$move_cd_y $(echo "$dj/`abs $dj`" |bc),"
+              move_cd_y="$move_cd_y $(echo "$dj_vitals/`abs $dj_vitals`" |bc),"
+              echo 'Move one step/grid in j direction.'
             else
-              move_cd_x="$move_cd_x $(echo "$di/`abs $di`" |bc),"
+              move_cd_x="$move_cd_x $(echo "$di_vitals/`abs $di_vitals`" |bc),"
               move_cd_y="$move_cd_y 0,"
+              echo 'Move one step/grid in i direction.'
             fi
           else
-            echo "The storm moves further in j direction thatn in i direction"
-            ds=$(echo "$s/`abs $di`" |bc)
-            if [ `echo "$t % $ds" |bc` == 0 ] && [ $t -le $(echo "`abs $di` * $ds" |bc) ]; then
-              move_cd_x="$move_cd_x $(echo "$di/`abs $di`" |bc),"
+            echo "The moving distance is further in j direction thatn in i direction"
+            ds=$(echo "$d_ij_vitals/`abs $di_vitals`" |bc)
+            if [ `echo "$step % $ds" |bc` == 0 ] && [ $step -le $(echo "`abs $di_vitals` * $ds" |bc) ]; then
+              move_cd_x="$move_cd_x $(echo "$di_vitals/`abs $di_vitals`" |bc),"
               move_cd_y="$move_cd_y 0,"
+              echo 'Move one step/grid in i direction.'
             else
               move_cd_x="$move_cd_x 0,"
-              move_cd_y="$move_cd_y $(echo "$dj/`abs $dj`" |bc),"
+              move_cd_y="$move_cd_y $(echo "$dj_vitals/`abs $dj_vitals`" |bc),"
+              echo 'Move one step/grid in j direction.'
             fi
           fi
-        elif [ `abs $di` -gt 0 ]; then
-          echo "The storm only moves in i direction between the two TCvital records"
-          move_cd_x="$move_cd_x $(echo "$di/`abs $di`" |bc),"
+        elif [ `abs $di_vitals` -gt 0 ] && [ `abs $dj_vitals` -eq 0 ]; then
+          echo "The move happens only in i direction between the two TCvital records"
+          move_cd_x="$move_cd_x $(echo "$di_vitals/`abs $di_vitals`" |bc),"
           move_cd_y="$move_cd_y 0,"
+          echo 'Move one step/grid in i direction.'
         else
-          echo "The storm does not move between the two TCvital records"
+          echo "The move happens only in j direction between the two TCvital records"
           move_cd_x="$move_cd_x 0,"
-          move_cd_y="$move_cd_y $(echo "$dj/`abs $dj`" |bc),"
+          move_cd_y="$move_cd_y $(echo "$dj_vitals/`abs $dj_vitals`" |bc),"
+          echo 'Move one step/grid in j direction.'
         fi
       fi
-
     done
   else
-    dt=`echo "$dt + $dmin" |bc`
+    dt_from1stVital=`echo "$dt_from1stVital + $dmin_vitals" |bc`
   fi
 done
 
+# Sanity Check 
+echo " "
+echo "--------------------------------------------------------------------------------------"
+echo "Sanity check the final location"
+echo "--------------------------------------------------------------------------------------"
+# split string and store it into an array
+IFS=', ' read -ra move_x_arr <<< "$move_cd_x"
+IFS=', ' read -ra move_y_arr <<< "$move_cd_y"
+# accumulate the steps
+total_xmove_D2=$(IFS=+; echo "$((${move_x_arr[*]}))")
+total_ymove_D2=$(IFS=+; echo "$((${move_y_arr[*]}))")
+total_xmove_D1=$(echo "$total_xmove_D2/${GRID_RATIO[1]}" |bc)
+total_ymove_D1=$(echo "$total_ymove_D2/${GRID_RATIO[1]}" |bc)
+# locate the final location in ij space in D1
+i_final_D1=$(echo "${ni[0]}+$total_xmove_D1" | bc)
+j_final_D1=$(echo "${nj[0]}+$total_ymove_D1" | bc)
+echo "Final calculated location in the index space for D1 is" $i_final_D1 $j_final_D1
+# Get lat lon of the final location 
+cat << EOF > ij2ll_final.ncl
+begin
+   opt = True
+   opt@MAP_PROJ          = 3
+   opt@TRUELAT1          = $TRUELAT1
+   opt@TRUELAT2          = $TRUELAT2
+   opt@STAND_LON         = $STAND_LON
+   opt@REF_LAT           = $REF_LAT
+   opt@REF_LON           = $REF_LON
+   opt@KNOWNI            = `echo "${E_WE[0]}/2" |bc -l`
+   opt@KNOWNJ            = `echo "${E_SN[0]}/2" |bc -l`
+   opt@DX                = ${DX[0]} 
+   opt@DY                = ${DY[0]}
+   loc=wrf_ij_to_ll(${i_final_D1},${j_final_D1},opt)
+   asciiwrite("ll_final",round(loc,3))
+end
+EOF
+ncl ij2ll_final.ncl
+echo "Final lon->`cat ll_final |head -n1`"
+echo "Final lat->`cat ll_final |tail -n1`"
+
+echo " "
+echo "--------------------------------------------------------------------------------------"
+echo "Writing output..."
+echo "--------------------------------------------------------------------------------------"
 corral_dist="8, 8,"
 for i in `seq 3 $MAX_DOM`; do
   corral_dist="$corral_dist $(echo "`min ${I_PARENT_START[$i-1]} ${J_PARENT_START[$i-1]}`-2" |bc),"
-  echo "The distance that the moving nest is allowed to get near the mother domain boundary: "  ${corral_dist[@]}
+  echo "The distance that the moving nest (D3) is allowed to get near the mother domain (D2)  boundary: "  ${corral_dist[@]}
 done
 
 # Output
