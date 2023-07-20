bec60d125cf53c594391dc47133246f0c7e23792
Judy-Yao <yao.zhu.91@gmail.com>
2023-07-20 11:50:23 -0500

    Add scripts to study the deep drop in pressure near the storm center

---
+++ Post_WRF_EnKF/Vis/Model/deep_slp_drop/Track_xbxa.py
@@ -0,0 +1,287 @@
+#!/work2/06191/tg854905/stampede2/opt/anaconda3/lib/python3.7
+
+import os,fnmatch # functions for interacting with the operating system
+import numpy as np
+from datetime import datetime, timedelta
+import glob
+import pickle
+from netCDF4 import Dataset
+from wrf import getvar 
+# It might be possible that you are not able to conda install wrf-var with a pretty new python version
+# Solution:
+# 1. conda create -n $PYTHON34_ENV_NAME python=3.4 anaconda 
+# 2. conda activate python=3.4 (use wrf-python in this python environment)
+import math
+import scipy as sp
+import scipy.ndimage
+import matplotlib
+import matplotlib.ticker as mticker
+from matplotlib import pyplot as plt
+from cartopy import crs as ccrs
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+from itertools import chain
+import time
+
+import Util_data as UD
+
+matplotlib.rcParams['xtick.direction'] = 'in'
+matplotlib.rcParams['ytick.direction'] = 'in'
+matplotlib.rcParams['xtick.top'] = True
+matplotlib.rcParams['ytick.right'] = True
+matplotlib.rcParams['lines.linewidth'] = 2.5#1.5
+matplotlib.rcParams['lines.markersize'] = 2.5
+matplotlib.rcParams['lines.markeredgewidth'] = 0
+matplotlib.rcParams['font.size'] = 15#6
+
+
+# ------------------------------------------------------------------------------------------------------
+#           Object: Analyses (wrf_enkf_output_d03_mean); Operation: Read and Process  
+# ------------------------------------------------------------------------------------------------------
+
+# Get analyses' estimated HPI 
+def read_HPI_model( Exper_name, file_kind ):
+    
+    DAtimes_dir =  sorted(glob.glob(big_dir+Storm+'/'+Exper_name+'/fc/20*') )
+    # remove the first directory (spin-up)
+    DAtimes_dir.pop(0)
+
+    DAtime_str = []
+    max_wind = []
+    min_slp = []
+    lat_storm = []
+    lon_storm = []
+       
+    # Read through analyses of the whole ensemble
+    for it_dir in DAtimes_dir:
+        # get DA time
+        DAtime_str.append( os.path.split(it_dir)[1])
+
+        filename = it_dir + '/' + file_kind
+        if not os.path.exists( filename ):
+            continue
+        with Dataset(filename) as ncid:
+            #print('Reading ', filename)
+            # maximum wind
+            ws = np.sqrt( ncid.variables['U10'][:]**2 + ncid.variables['V10'][:]**2 )
+            max_wind.append( np.max( ws ))
+            # minimum sea level pressure
+            slp = getvar(ncid, 'slp')
+            min_slp.append( np.min( slp )) 
+            # location of the minimum slp
+            slp_smooth = sp.ndimage.filters.gaussian_filter(slp, [11, 11] )
+            idx = np.nanargmin( slp_smooth )
+            lat_storm.append( ncid.variables['XLAT'][:].flatten()[idx]) 
+            lon_storm.append( ncid.variables['XLONG'][:].flatten()[idx]) 
+
+    HPI_model = {'time':DAtime_str, 'lat': lat_storm, 'lon':lon_storm, 'max_ws': max_wind, 'min_slp':min_slp}
+
+    return HPI_model
+
+# ------------------------------------------------------------------------------------------------------
+#            Operation: Plot
+# ------------------------------------------------------------------------------------------------------
+def plot_one( ax0, ax1, ax2,  state, color, style, line_width, label, steps=1):
+ 
+    if label == 'Best track':
+        times = state['time']
+        lon = state['lon']
+        lat = state['lat']
+        x_min_slp = state['min_slp']
+        x_max_ws = state['max_ws']
+
+        ax0.plot(lon, lat, marker='o',markersize=3, color=color,linewidth=3, label=label, linestyle=style, transform=ccrs.PlateCarree())
+        dates = [datetime.strptime(i,"%Y%m%d%H%M") for i in times]
+        ax1.plot_date(dates, x_min_slp, color, label=label, linewidth=3)
+        ax2.plot_date(dates, x_max_ws, color, label=label, linewidth=3)
+
+        for it in times:
+            if it[8:10] == '00':
+                boolean_compare = [ it  == eachT for eachT in times ]
+                idx = int( np.where(boolean_compare)[0] )
+                ax0.scatter( lon[idx], lat[idx],s=5, marker='o',edgecolor="white",transform=ccrs.PlateCarree())
+                ax0.annotate(it[6:8], xy=(lon[idx], lat[idx]), color=color, xycoords='data', transform=ccrs.PlateCarree())
+        
+    else:
+        # Plot track for xb and xa
+        for key in state.keys():
+            lon = state[key]['lon'][::steps]
+            lat = state[key]['lat'][::steps]
+            if key == 'wrf_enkf_input_d03_mean':
+                ax0.plot(lon, lat, marker='o', markersize=3, color=color,linewidth=line_width, label='Xb'+label, linestyle='-', transform=ccrs.PlateCarree())
+            else:
+                ax0.plot(lon, lat, marker='o', markersize=3, color=color,linewidth=line_width, label='Xa'+label, linestyle='--', transform=ccrs.PlateCarree())
+        # Plot zipped HPI of xb and xa
+        times = state['wrf_enkf_input_d03_mean']['time'][::steps]
+        dates = [datetime.strptime(i,"%Y%m%d%H%M") for i in times]
+        dates_zip = list( chain.from_iterable( zip(dates,dates)) )
+
+        xb_min_slp = state['wrf_enkf_input_d03_mean']['min_slp'][::steps]
+        xa_min_slp = state['wrf_enkf_output_d03_mean']['min_slp'][::steps]
+        min_slp_zip = list( chain.from_iterable( zip(xb_min_slp,xa_min_slp)) )
+
+        xb_max_ws = state['wrf_enkf_input_d03_mean']['max_ws'][::steps]
+        xa_max_ws = state['wrf_enkf_output_d03_mean']['max_ws'][::steps]
+        max_ws_zip = list( chain.from_iterable( zip(xb_max_ws,xa_max_ws)) )
+
+        ax1.plot_date(dates_zip, min_slp_zip, color, linewidth=line_width )
+        ax2.plot_date(dates_zip, max_ws_zip, color, label=label, linewidth=line_width )
+
+
+def Plot_HPI_xbxa(  domain_range ):
+
+    # Set the end point of the period to investigate
+    if Storm == 'HARVEY':
+        DF_model_end  = '201708251200'
+    elif Storm == 'IRMA':
+        DF_model_end  = '201709050000'
+    elif Storm == 'MARIA':
+        DF_model_end  = '201709180000'
+    elif Storm == 'JOSE':
+        DF_model_end  = '201709070000'
+    else:
+        DF_model_end  = None
+
+    # Set up figure
+    fig = plt.figure( figsize=(22,6.5), dpi=150 ) #12,4 20,6
+    gs = fig.add_gridspec(4,7) # 2,7
+
+    ax0 = fig.add_subplot( gs[:2,0:3],  projection=ccrs.PlateCarree())
+    ax0.set_extent( domain_range,  crs=ccrs.PlateCarree())
+    ax0.coastlines( resolution='10m', color='black',linewidth=0.5 )
+    ax1 = fig.add_subplot( gs[2:,3:5] )
+    ax2 = fig.add_subplot( gs[2:,5:7] )
+    ax3 = fig.add_subplot( gs[2:,:] )
+
+    # Set start and end of the period
+    if Storm == 'HARVEY':
+        Btk_start = '201708220600' # '201709161800' #'201709030600'
+        Btk_end = '201708251800' # '201709210000' #'201709090000'
+    elif Storm == 'IRMA':
+        Btk_start = '201709021800'
+        Btk_end = '201709050600'
+    elif Storm == 'MARIA':
+        Btk_start = '201709151800'#'201709160000'
+        Btk_end = '201709180600'
+    elif Storm == 'JOSE':
+        Btk_start = '201709041800'
+        Btk_end = '201709070600'
+    else:
+        pass
+
+    # Plot HPI from post-storm analysis
+    best_track = UD.btk_in_duration(Storm, Btk_start, Btk_end, hour_step=6)
+    plot_one ( ax0, ax1, ax2, best_track,  'black', '-', 3, 'Best track' )
+
+    Colors = ['#e6194B','#4363d8',] #'#748b97'
+    Line_types = ['-','-']
+    Labels = MP
+
+    # Plot HPI from model
+    for iExper in Exper_names:
+        print('Plotting '+iExper)
+        idx_exper = Exper_names.index( iExper )
+        HPI_models = {}
+        file_kinds = ['wrf_enkf_input_d03_mean','wrf_enkf_output_d03_mean']
+        for ifk in file_kinds:
+            idx = file_kinds.index( ifk )
+            HPI_models[ifk] = read_HPI_model( iExper, ifk )
+        plot_one ( ax0, ax1, ax2, HPI_models, Colors[idx_exper], '-',  1.5, Labels[idx_exper], steps=1 )
+        # Plot increment
+        times = HPI_models['wrf_enkf_input_d03_mean']['time']
+        dates = [datetime.strptime(i,"%Y%m%d%H%M") for i in times]
+        xb_min_slp = HPI_models['wrf_enkf_input_d03_mean']['min_slp']
+        xa_min_slp = HPI_models['wrf_enkf_output_d03_mean']['min_slp']
+        xb_max_ws = HPI_models['wrf_enkf_input_d03_mean']['max_ws']
+        xa_max_ws = HPI_models['wrf_enkf_output_d03_mean']['max_ws']
+        incre_slp = np.array(xa_min_slp) - np.array(xb_min_slp)
+        ax3.plot_date(times, incre_slp, Colors[idx_exper], 1.5, Labels[idx_exper])
+
+    # Set ticks/labels for track subplot
+    lon_ticks = list(range(math.ceil(domain_range[0])-2, math.ceil(domain_range[1])+2, 4))
+    lat_ticks = list(range(math.ceil(domain_range[2])-2, math.ceil(domain_range[3])+2, 2))
+    gl = ax0.gridlines(crs=ccrs.PlateCarree(), draw_labels=False,linewidth=0.1, color='gray', alpha=0.5, linestyle='--')
+    gl.ylabels_left = True
+    gl.xlabels_bottom = True
+    gl.ylocator = mticker.FixedLocator(lat_ticks)
+    gl.xlocator = mticker.FixedLocator(lon_ticks)
+    gl.yformatter = LATITUDE_FORMATTER
+    gl.xformatter = LONGITUDE_FORMATTER
+    gl.xlabel_style = {'size': 15}
+    gl.ylabel_style = {'size': 15}  
+   
+    # Set ticks for intensity subplots
+    ax1.set_xlim([datetime(int(Btk_start[0:4]), int(Btk_start[4:6]), int(Btk_start[6:8]), int(Btk_start[8:10])), datetime(int(Btk_end[0:4]), int(Btk_end[4:6]), int(Btk_end[6:8]), int(Btk_end[8:10]))])
+    ax2.set_xlim([datetime(int(Btk_start[0:4]), int(Btk_start[4:6]), int(Btk_start[6:8]), int(Btk_start[8:10])), datetime(int(Btk_end[0:4]), int(Btk_end[4:6]), int(Btk_end[6:8]), int(Btk_end[8:10]))])
+    ax1.set_ylim([900,1020])     #([940, 1015])
+    ax2.set_ylim([10,80])   #([10,60])
+    ax1.tick_params(axis='x', labelrotation=30,labelsize=12)
+    ax2.tick_params(axis='x', labelrotation=30,labelsize=12)
+    ax2.legend(bbox_to_anchor=(1.43, 1.0),frameon=True,loc='upper right',fontsize='15')
+    ax3.set_xlim([datetime(int(Btk_start[0:4]), int(Btk_start[4:6]), int(Btk_start[6:8]), int(Btk_start[8:10])), datetime(int(Btk_end[0:4]), int(Btk_end[4:6]), int(Btk_end[6:8]), int(Btk_end[8:10]))])
+    ax3.set_ylim([0,100])            
+    ax3.set_ylabel('Increment of MSLP (hPa)',fontsize = 15)
+ 
+    # Set titles
+    ax0.set_title( 'Track',fontsize = 18 )
+    ax1.set_title( 'MSLP (hPa)',fontsize = 18 )
+    ax2.set_title( 'Vmax ($\mathregular{ms^{-1}}$)',fontsize = 18 )
+    fig.suptitle( Storm+': '+DA,fontsize = 18)
+
+    des_name = small_dir+Storm+'/'+Exper_names[0]+'/Vis_analyze/Model/'+Storm+'_'+DA+'_xbxa_HPI.png'
+    plt.savefig( des_name )
+    print( 'Saving the figure to '+des_name+'!' )
+
+if __name__ == '__main__':
+    
+    big_dir = '/scratch/06191/tg854905/Pro2_PSU_MW/'
+    small_dir =  '/work2/06191/tg854905/stampede2/Pro2_PSU_MW/'
+
+    # ---------- Configuration -------------------------
+    Storm = 'IRMA'
+    DA = 'IR'
+    MP = ['WSM6',]
+
+    # Pre-set the domain for DF forecast
+    if Storm == 'HARVEY':
+        lon_min = -102
+        lon_max = -85
+        lat_min = 16
+        lat_max = 31
+    elif Storm == 'IRMA':
+        lon_min = -58
+        lon_max = -44
+        lat_min = 14
+        lat_max = 24
+    elif Storm == 'MARIA':
+        lon_min = -68
+        lon_max = -43#-45
+        lat_min = 5# 
+        lat_max = 25
+    elif Storm == 'JOSE':
+        lon_min = -65
+        lon_max = -35#-45
+        lat_min = 0
+        lat_max = 25
+    else:
+        pass
+    domain_range = [lon_min, lon_max, lat_min, lat_max]
+    # -------------------------------------------------------  
+
+    # Create experiment names
+    Exper_names = []
+    for imp in MP:
+        Exper_names.append( UD.generate_one_name( Storm,DA,imp ) )  
+
+    start_time=time.process_time()
+    Plot_HPI_xbxa( domain_range )
+    end_time = time.process_time()
+    print ('time needed: ', end_time-start_time, ' seconds')
+
+
+
+
+
+
+
+
+
---
+++ Post_WRF_EnKF/Vis/Model/deep_slp_drop/hydro_profile_xbxa.py
@@ -0,0 +1,247 @@
+#!/work2/06191/tg854905/stampede2/opt/anaconda3/lib/python3.7
+
+from numba import njit, prange
+import os # functions for interacting with the operating system
+import numpy as np
+from datetime import datetime, timedelta
+import glob
+import netCDF4 as nc
+from wrf import getvar, ll_to_xy
+import math
+import scipy as sp
+import scipy.ndimage
+import matplotlib
+matplotlib.use("agg")
+import matplotlib.ticker as mticker
+from matplotlib import pyplot as plt
+import matplotlib.colors as mcolors
+from cartopy import crs as ccrs
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import time
+from scipy import interpolate
+
+import Util_data as UD
+
+
+# ------------------------------------------------------------------------------------------------------
+#           Operation: Perform Operations
+# ------------------------------------------------------------------------------------------------------
+@njit(parallel=True)
+def hydro_mass( full_p, tv, geoHm, q ):
+    R = 287.06
+    res = np.zeros( (q.shape[0],q.shape[1]), )
+    res[:] = np.nan
+    for im in prange( q.shape[1] ):
+        for il in range( q.shape[0] ):
+            zdiff = geoHm[il+1,im] - geoHm[il,im]
+            res[il,im] = (full_p[il,im]/(R * tv[il,im])) * q[il,im] * zdiff #* model_resolution**2
+    # make sure all values are reasonable
+    assert res.any() != np.nan
+    return res
+
+
+
+# Return: for each var, a 3d field with dimension: number of files, 
+# & number of levels, and number of points in the area
+def compute_hydro_area( wrf_files, hydros):
+
+    # Dimension of the domain
+    nLevel = 42
+
+    d_hydro = {}
+    
+    # Find the min slp in enkf output as the anchor point
+    output_file = wrf_files[1]
+    ncdir = nc.Dataset( output_file )
+    slp_original = UD.compute_slp( ncdir ).flatten()
+    min_slp = np.amin( slp_original )
+    idx_anchor = np.where(slp_original == min_slp)[0]
+    # Find the circled area with the min slp as the center 
+    # & read the common variables: xlat/xlon
+    xlat = ncdir.variables['XLAT'][0,:,:].flatten()
+    xlon = ncdir.variables['XLONG'][0,:,:].flatten()
+    anchor_ij = ll_to_xy(ncdir, xlat[idx_anchor], xlon[idx_anchor])
+    idx_area = UD.find_circle_area( output_file, anchor_ij.values[0], anchor_ij.values[1], 200, model_resolution/1000) # in km
+     #print(len(idx_area))
+
+    # calculate fields needed for mass of hydrometeors
+    full_p = np.zeros( [len(wrf_files), nLevel, len(idx_area)] )
+    tv_k = np.zeros( [len(wrf_files), nLevel, len(idx_area)] )
+    geoHm = np.zeros( [len(wrf_files), nLevel+1, len(idx_area)] ) 
+    for wrf_file in wrf_files:
+        ifile = wrf_files.index(wrf_file)
+        ncdir = nc.Dataset( wrf_file, 'r')
+        # full pressure
+        p = ncdir.variables['P'][0,:,:,:] # perturbation
+        pb = ncdir.variables['PB'][0,:,:,:]
+        tmp_p = p + pb
+        tmp_p = tmp_p.reshape( tmp_p.shape[0],-1 )
+        full_p[ifile,:,:] = tmp_p[:,idx_area]
+        # geopotential height
+        ph = ncdir.variables['PH'][0,:,:,:] # perturbation
+        phb = ncdir.variables['PHB'][0,:,:,:]
+        tmp_geoHm = (ph+phb)/9.81 # in meter
+        tmp_geoHm = tmp_geoHm.reshape( tmp_geoHm.shape[0],-1 )
+        geoHm[ifile,:,:] = tmp_geoHm[:,idx_area]
+        # virtual temperature
+        tv = getvar(ncdir,'tv',units='K')
+        tmp_tv = tv.values
+        tmp_tv = tmp_tv.reshape( tmp_tv.shape[0],-1 )
+        tv_k[ifile,:,:] = tmp_tv[:,idx_area]
+    # calculate the mass of hydrometeor per layer
+    for var_name in hydros:
+        hydro_layer = np.zeros( [len(wrf_files), nLevel, len(idx_area)] )
+        hydro_layer[:] = np.nan
+        for wrf_file in wrf_files:
+            ifile = wrf_files.index(wrf_file)
+            ncdir = nc.Dataset( wrf_file, 'r')
+            ivar = hydros.index(var_name)
+            var = ncdir.variables[var_name][0,:,:,:]
+            var = var.reshape( var.shape[0],-1 )
+            hydro_layer[ifile,:,:] = hydro_mass( full_p[ifile,:,:], tv_k[ifile,:,:], geoHm[ifile,:,:], var[:,idx_area] )
+        d_hydro[var_name] = hydro_layer
+
+    # Calculate the vertical coordinate
+    Height = np.mean( geoHm, axis=0) 
+    Height = np.mean( Height, axis=1 )/1000 # convert to km
+    Height = (Height[:-1]+Height[1:])/2
+    return d_hydro, Height
+
+# Plot height profile of the sum of hydrometeors
+def plot_PF_all(DAtime, plot_dir, d_hydro, Height):
+
+
+    # Set up figure
+    fig = plt.figure( figsize=(12,6), dpi=300 )
+    ax = plt.subplot(1,1,1)
+
+    # Manually set discrete values on x and y axis and interpolate data to these values
+    ## x axis: correlation value
+    x_range = np.arange(0,2100.5,50)
+    x_axis_rg = range(len(x_range))
+    f_xinterp = interpolate.interp1d( x_range, x_axis_rg)
+    ## y axis: model level height
+    y_range = np.arange(0,31,1)
+    y_axis_rg = range(len(y_range))
+    f_yinterp = interpolate.interp1d( y_range, y_axis_rg)
+    loc_iny = f_yinterp( Height )
+
+    labels = ['Xb_','Xa_']
+    lstyle = ['-','--']
+    Color = ['#f032e6','#911eb4','#4363d8','#f58231','#469990']
+
+    for ifile in range( len(wrf_files) ):
+        for var in hydros:
+            idx = hydros.index( var )
+            PF_x = np.sum(d_hydro[var][ifile,:,:],axis=1)
+            loc_inx = f_xinterp( PF_x )
+            ax.plot( loc_inx,loc_iny,Color[idx],linewidth=2,label=labels[ifile]+var,linestyle=lstyle[ifile] )
+
+    # set lables
+    ax.legend(loc='upper right',fontsize='10')
+    # set X label
+    ax.set_xticks( x_axis_rg[::5] )
+    ax.set_xticklabels( ['0','250','500','750','1000','1250','1500','2000'],fontsize=15 ) 
+    ax.set_xlabel('Mass (kg m-2)',fontsize=15)
+    ax.set_xlim(xmin=0) 
+    # set Y label
+    ylabel_like = [0.0,5.0,10.0,15.0,20.0]
+    yticks = []
+    list_y_range = list(y_range)
+    for it in ylabel_like:
+        yticks.append( list_y_range.index(it) )
+    ax.set_yticks( yticks )
+    ax.set_yticklabels( [str(it) for it in ylabel_like],fontsize=15 )
+    ax.set_ylabel('Height (km)',fontsize=15)
+    ax.set_ylim(ymin=0,ymax=20.5) # cut off data above 25km
+
+    # Set title
+    title_name = 'Profile: mass of hydrometeors in the area'
+    ax.set_title( title_name,fontweight="bold",fontsize='12' )
+    fig.suptitle(Storm+': '+Exper_name+' '+DAtime , fontsize=10, fontweight='bold')
+
+    # Save the figure
+    save_des = plot_dir+DAtime+'_VP_each_hydro_area.png'
+    plt.savefig( save_des )
+    print( 'Saving the figure: ', save_des )
+    plt.close()
+
+
+    return None
+
+
+
+
+if __name__ == '__main__':
+
+    big_dir = '/scratch/06191/tg854905/Pro2_PSU_MW/'
+    small_dir =  '/work2/06191/tg854905/stampede2/Pro2_PSU_MW/'
+    model_resolution = 3000 #m
+    # ---------- Configuration -------------------------
+    Storm = 'IRMA'
+    Exper_name = 'IR-J_DA+J_WRF+J_init-SP-intel17-WSM6-30hr-hroi900'
+    hydros = ['QCLOUD','QRAIN','QICE','QSNOW','QGRAUP']
+
+    # Time range set up
+    start_time_str = '201708221200'
+    end_time_str = '201708221200'
+    Consecutive_times = False
+
+    each_water = True
+    Plot_PF = True
+    Plot_all = True # all species together
+    # -------------------------------------------------------    
+
+    if not Consecutive_times:
+        DAtimes = ['201709041400','201709041500']
+        #DAtimes = ['201708230000','201708230600','201708231200','201708231800','201708240000','201708240600','201708241200']
+        #DAtimes = ['201709031200','201709031800','201709040000','201709040600','201709041200','201709041800','201709050000']
+        #DAtimes = ['201709161200','201709161800','201709170000','201709170600','201709171200','201709171800','201709180000']
+    else:
+        time_diff = datetime.strptime(end_time_str,"%Y%m%d%H%M") - datetime.strptime(start_time_str,"%Y%m%d%H%M")
+        time_diff_hour = time_diff.total_seconds() / 3600
+        time_interest_dt = [datetime.strptime(start_time_str,"%Y%m%d%H%M") + timedelta(hours=t) for t in list(range(0, int(time_diff_hour)+1, 1))]
+        DAtimes = [time_dt.strftime("%Y%m%d%H%M") for time_dt in time_interest_dt]
+
+
+    # Plot integrated column of hydrometeors
+    if Plot_PF:
+        start_time=time.process_time()
+        for DAtime in DAtimes:
+            print('At '+DAtime)
+            wrf_dir = big_dir+Storm+'/'+Exper_name+'/fc/'+DAtime+'/'
+            wrf_files = [wrf_dir+'/wrf_enkf_input_d03_mean',wrf_dir+'/wrf_enkf_output_d03_mean']
+            d_hydro,Height = compute_hydro_area( wrf_files, hydros)
+            # ------ Plot -------------------
+            plot_dir = small_dir+Storm+'/'+Exper_name+'/Vis_analyze/Model/VProfile_Hydro_mass/'
+            plotdir_exists = os.path.exists( plot_dir )
+            if plotdir_exists == False:
+                os.mkdir(plot_dir)
+            # --- Condition
+            if each_water:
+                v_interest = hydros
+                if not Plot_all:
+                    for var in v_interest:
+                        print('Plotting the profile of '+var)
+                        #PF_xb = np.sum(d_hydro[var][0,:,:],axis=1) 
+                        #PF_xa = np.sum(d_hydro[var][1,:,:],axis=1)
+                        plot_PF_each(DAtime, plot_dir, d_hydro, var )
+                else:
+                   plot_PF_all(DAtime, plot_dir, d_hydro, Height ) 
+            else:
+                pass
+                #v_interest = ['liquid','ice','all_hydro']
+                #for var in v_interest:
+                #    if var == 'liquid':
+                #        tmp = d_hydro['QCLOUD']+d_hydro['QRAIN']
+                #    elif var == 'ice':
+                #        tmp = d_hydro['QPFE']+d_hydro['QSNOW']+d_hydro['QGRAUP']
+                #    elif var == 'all_hydro':
+                #        tmp = d_hydro['QCLOUD']+d_hydro['QRAIN']+d_hydro['QPFE']+d_hydro['QSNOW']+d_hydro['QGRAUP']
+                #    PF_xb = np.sum( tmp[0,:,:],axis=0 )
+                #    PF_xa = np.sum( tmp[1,:,:],axis=0 )
+                #    plot_PF(DAtime, plot_dir, d_hydro, var, PF_xb, PF_xa, )
+
+        end_time = time.process_time()
+        print ('time needed: ', end_time-start_time, ' seconds')
